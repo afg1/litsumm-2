b407a10b8fbf57db95af45b5811f1ff1dd0c13b6
Andrew Green <agreen@ebi.ac.uk>
2023-11-01 09:45:18 +0000

    Make OLS search work on a batch as well

--- agent/agent_tools.py
+++ agent/agent_tools.py
@@ -1,3 +1,8 @@
+"""
+These are pure python tools that an agent can use in its reasoning.
+They need to be async to be callable in the lmql queries.
+They can return anything, but the returned values should be representable as a string somehow
+"""
 import requests
 from ingestion.from_epmc import get_sections, get_text
 from agent.settings import EUROPE_PMC, OLS4_API
@@ -29,29 +34,54 @@ async def search_ols(query, k=1):
     """
     Queries the OLS and retrieves the top k definitions and their term IDs
     """
-    query = re.sub('\d\.','',query).strip().lower()
-    print(query)
-    url = f"{OLS4_API}search?q={{{query}}}&ontology=bao"
-    print(url)
-    r = requests.get(url)
-
     term_ids = []
     descriptions = []
-    if r.ok:
-        data = r.json()['response']
-        if len(data['docs']) == 0:
-            return [], []
-        elif len(data['docs']) < k:
-            k = len(data['docs'])
-        for i in range(k):
-            if len(data['docs'][i]['description']) == 0:
-                continue
-            ontology_id = data['docs'][i]['obo_id']
-            description = data['docs'][i]['label'] + ": " + data['docs'][i]['description'][0]
-            term_ids.append(ontology_id)
-            descriptions.append(description)
+    if isinstance(query, list):
+        for q in query:
+            term_ids_q = []
+            descriptions_q = []
+            q = re.sub('\d\.','',q).strip().lower()
+            print(q)
+            url = f"{OLS4_API}search?q={{{q}}}&ontology=bao"
+            print(url)
+            r = requests.get(url)
+            if r.ok:
+                data = r.json()['response']
+                if len(data['docs']) == 0:
+                    return [], []
+                elif len(data['docs']) < k:
+                    k = len(data['docs'])
+                for i in range(k):
+                    if len(data['docs'][i]['description']) == 0:
+                        continue
+                    ontology_id = data['docs'][i]['obo_id']
+                    description = data['docs'][i]['label'] + ": " + data['docs'][i]['description'][0]
+                    term_ids_q.append(ontology_id)
+                    descriptions_q.append(description)
+            term_ids.append(term_ids_q)
+            descriptions.append(descriptions_q)
 
-    print(f"Found {len(term_ids)} terms for {query}")
+    elif isinstance(query, str):
+        query = re.sub('\d\.','',query).strip().lower()
+        print(query)
+        url = f"{OLS4_API}search?q={{{query}}}&ontology=bao"
+        print(url)
+        r = requests.get(url)
+        if r.ok:
+            data = r.json()['response']
+            if len(data['docs']) == 0:
+                return [], []
+            elif len(data['docs']) < k:
+                k = len(data['docs'])
+            for i in range(k):
+                if len(data['docs'][i]['description']) == 0:
+                    continue
+                ontology_id = data['docs'][i]['obo_id']
+                description = data['docs'][i]['label'] + ": " + data['docs'][i]['description'][0]
+                term_ids.append(ontology_id)
+                descriptions.append(description)
+
+        print(f"Found {len(term_ids)} terms for {query}")
     return term_ids, descriptions
 
 
