c1a05fba1ebd8295c7d959254561f38fa0e0d64d
Andrew Green <agreen@ebi.ac.uk>
2023-11-08 15:01:20 +0000

    Add agent settings

---
+++ agent/settings.py
@@ -0,0 +1,6 @@
+
+
+
+EUROPE_PMC = "https://www.ebi.ac.uk/europepmc/webservices/rest/"
+OLS4_API = "https://www.ebi.ac.uk/ols4/api/"
+LITSCAN_API = "https://wwwdev.ebi.ac.uk/ebisearch/ws/rest/rnacentral-litscan"
