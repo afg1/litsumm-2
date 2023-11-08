a0f762c4fe6c2efab315005f94066052c96c8375
Andrew Green <agreen@ebi.ac.uk>
2023-11-08 15:05:07 +0000

    Enable gpu offloading

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from agent.tools.find_method import evaluate_method
 @click.option("--output", default="output", help="Output filename")
 @click.option("--model", default="sciphi-mistral-7b-32k.Q4_K_M.gguf", help="Path to model file")
 @click.option("--tokenizer", default='mistralai/Mistral-7B-v0.1', help="Name of tokenizer")
-def main(input, output, model, tokenizer):
+@click.option("--n_gpu", default=1, help="Number of layers to offload (1 for Metal, ~40 for GPU)")
+def main(input, output, model, tokenizer, n_gpu):
     pmcids = [p.strip() for p in open(input, 'r').readlines()]
     print(pmcids)
-    model = lmql.model(f"local:llama.cpp:{model}", n_ctx=16384, n_gpu_layers=1, tokenizer=tokenizer)
+    model = lmql.model(f"local:llama.cpp:{model}", n_ctx=16384, n_gpu_layers=n_gpu, tokenizer=tokenizer)
     ## First process each pmcid and annotate it with some ontology terms
     results = {}
     for pmcid in tqdm(pmcids):
